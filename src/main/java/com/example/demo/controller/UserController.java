5fade2a6ce90be2f28a3b898275c4c9182584232
cloudtraining <cloudtraining@CTIO-MBookPro-3.local>
2019-11-18 15:21:42 -0600

    swagger

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.RestClientContext;
 import com.example.demo.models.User;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -26,7 +24,6 @@ public class UserController {
 
     @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAllUsers() {
-
 
         ResponseEntity<User[]> userEntity = restTemplate.getForEntity(URI, User[].class);
 
--- src/test/java/com/example/demo/models/UserDemoTest.java
+++ src/test/java/com/example/demo/models/UserDemoTest.java
@@ -1,8 +1,9 @@
 package com.example.demo.models;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserDemoTest {
 
